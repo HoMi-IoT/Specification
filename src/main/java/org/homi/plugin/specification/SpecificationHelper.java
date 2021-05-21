5e9e4cd2a5994f4b267e345cb64828da707e9843
Nicolas Hanout <nicolas.hanout@gmail.com>
2021-05-21 13:49:18 -0700

    fixed types casting

--- src/main/java/org/homi/plugin/specification/SpecificationHelper.java
+++ src/main/java/org/homi/plugin/specification/SpecificationHelper.java
@@ -4,15 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public final class SpecificationHelper {
-	public static Type<?>[] processTypes(Object[] parameterTypes) throws Exception {
-		List<Type<?>> types = new ArrayList<>();
-		for(Object parameterType: parameterTypes) {
-			types.add(processTypes(parameterType));
-		}
-		return (Type<?>[]) types.toArray();
-	}
+//	public static Type<?>[] processTypes(Object[] parameterTypes) throws Exception {
+//		List<Type<?>> types = new ArrayList<>();
+//		for(Object parameterType: parameterTypes) {
+//			types.add(processTypes(parameterType));
+//		}
+//		return (Type<?>[]) types.toArray();
+//	}
 	
-	public static Type<?> processTypes(Object parameterType) throws Exception {
+	public static Type<?> processType(Object parameterType) throws Exception {
 		if(parameterType instanceof Type)
 			return (Type<?>) parameterType;
 
