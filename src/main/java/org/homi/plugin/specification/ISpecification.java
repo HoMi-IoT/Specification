f6803ddce78f591b06ec0dd91753b65b33b228e8
Nicolas Hanout <nicolas.hanout@gmail.com>
2021-05-26 15:41:32 -0700

    removed comments

--- src/main/java/org/homi/plugin/specification/ISpecification.java
+++ src/main/java/org/homi/plugin/specification/ISpecification.java
@@ -2,30 +2,12 @@ package org.homi.plugin.specification;
 
 import java.util.List;
 
-/**
- * 
- * @author Nicolas Hanout
- *
- * @since 0.0.1
- */
 public interface ISpecification {
 	
-	/**
-	 * 
-	 * @return
-	 */
 	public String name();
 	
-	/**
-	 * 
-	 * @return
-	 */
 	public List<TypeDef<?>> getParameterTypes();
 	
-	/**
-	 * 
-	 * @return
-	 */
-	public TypeDef<?> getReturnType();;
+	public TypeDef<?> getReturnType();
 	
 }
