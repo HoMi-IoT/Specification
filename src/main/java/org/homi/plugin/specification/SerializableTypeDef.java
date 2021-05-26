a11514e60b3d813e9db4b55b3c7de27f0baf5e02
Nicolas Hanout <nicolas.hanout@gmail.com>
2021-05-26 15:41:14 -0700

    refactored SerializableTypeDef

--- src/main/java/org/homi/plugin/specification/SerializableTypeDef.java
+++ src/main/java/org/homi/plugin/specification/SerializableTypeDef.java
@@ -12,35 +12,44 @@ import org.homi.plugin.specification.exceptions.InvalidArgumentException;
 
 public class SerializableTypeDef<T extends Serializable> extends TypeDef<T> {
 
-	public SerializableTypeDef(Class<T> type, Predicate<T>...constraints) {
+	public SerializableTypeDef(Class<T> type, Predicate<T>... constraints) {
 		super(type, constraints);
 	}
-	
+
 	@Override
 	public T process(Object obj) throws InvalidArgumentException {
-		if(!(obj instanceof Serializable)){
-			throw new InvalidArgumentException("Expected argument of type ["+Serializable.class.getName()+" but received type"+ Object.class.getName() +"]");
+		if (!(obj instanceof Serializable)) {
+			throw new InvalidArgumentException("Expected argument of type [" + Serializable.class.getName()
+					+ " but received type" + Object.class.getName() + "]");
 		}
+		return srializaThenDecerialize(obj);
+	}
 
+	private T srializaThenDecerialize(Object obj) throws InvalidArgumentException {
 		try {
-			ByteArrayOutputStream baos = new  ByteArrayOutputStream();
-			ObjectOutputStream oos = new ObjectOutputStream(baos);
-				oos.writeObject(obj);
-			byte[] serial = baos.toByteArray();
-			oos.close();
-			baos.close();
-
-			ByteArrayInputStream bais = new  ByteArrayInputStream(serial);
-			ObjectInputStream ois;
-				ois = new ObjectInputStream(bais);
-			
-			Object temp = ois.readObject();
-			ois.close();
-			bais.close();
+			byte[] serial = serialize(obj);
+			Object temp = decerialize(serial);
 			return this.getType().cast(temp);
 		} catch (IOException | ClassNotFoundException e) {
-			throw new InvalidArgumentException("Error serializing  type ["+Serializable.class.getName()+" to "+ Object.class.getName() +"]", e);
+			throw new InvalidArgumentException(
+					"Error serializing  type [" + Serializable.class.getName() + " to " + Object.class.getName() + "]", e);
 		}
 	}
 
+	private byte[] serialize(Object obj) throws IOException {
+		try (ByteArrayOutputStream baos = new ByteArrayOutputStream();
+				ObjectOutputStream oos = new ObjectOutputStream(baos)) {
+			oos.writeObject(obj);
+			return baos.toByteArray();
+		}
+	}
+	
+	private Object decerialize(byte[] serial) throws IOException, ClassNotFoundException {
+		try (ByteArrayInputStream bais = new ByteArrayInputStream(serial);
+				ObjectInputStream ois = new ObjectInputStream(bais)) {
+			Object temp = ois.readObject();
+			return temp;
+		}
+	}
+	
 }
