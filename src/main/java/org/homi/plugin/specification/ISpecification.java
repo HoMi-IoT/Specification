63861a886313845888d297a2c2a346df8ce85a13
Nicolas Hanout <nicolas.hanout@gmail.com>
2021-05-27 09:23:33 -0700

    added default method to get the id in the ISpecification interface

--- src/main/java/org/homi/plugin/specification/ISpecification.java
+++ src/main/java/org/homi/plugin/specification/ISpecification.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 public interface ISpecification {
 	
+	public default String id() {
+		return this.getClass().getAnnotation(SpecificationID.class).id();
+	}
+	
 	public String name();
 	
 	public List<TypeDef<?>> getParameterTypes();
